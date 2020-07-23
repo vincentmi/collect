696957024499ae7e93b2f34b1b4d702b1d8afecc
Vincent Mi <miwenshu@gmail.com>
2020-07-24 00:17:38 +0800

    1

--- json_rpc_tool/rpc.php
+++ json_rpc_tool/rpc.php
@@ -356,87 +356,6 @@ function displayForm()
 </body>
 <script>
 
-var formatJson = function(json, options) {
-    var reg = null,
-        formatted = "",
-        pad = 0,
-        PADDING = "    "; // one can also use "\t" or a different number of spaces
-
-    // optional settings
-    options = options || {};
-    // remove newline where "{" or "[" follows ":"
-    options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
-    // use a space after a colon
-    options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
-
-    // begin formatting...
-    if (typeof json !== "string") {
-        // make sure we start with the JSON as a string
-        json = JSON.stringify(json);
-    } else {
-        // is already a string, so parse and re-stringify in order to remove extra whitespace
-        json = JSON.parse(json);
-        json = JSON.stringify(json);
-    }
-
-    // add newline before and after curly braces
-    reg = /([\{\}])/g;
-    json = json.replace(reg, "\r\n$1\r\n");
-
-    // add newline before and after square brackets
-    reg = /([\[\]])/g;
-    json = json.replace(reg, "\r\n$1\r\n");
-
-    // add newline after comma
-    reg = /(\,)/g;
-    json = json.replace(reg, "$1\r\n");
-
-    // remove multiple newlines
-    reg = /(\r\n\r\n)/g;
-    json = json.replace(reg, "\r\n");
-
-    // remove newlines before commas
-    reg = /\r\n\,/g;
-    json = json.replace(reg, ",");
-
-    // optional formatting...
-    if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
-        reg = /\:\r\n\{/g;
-        json = json.replace(reg, ":{");
-        reg = /\:\r\n\[/g;
-        json = json.replace(reg, ":[");
-    }
-    if (options.spaceAfterColon) {
-        reg = /\:/g;
-        json = json.replace(reg, ": ");
-    }
-
-    $.each(json.split("\r\n"), function(index, node) {
-        var i = 0,
-            indent = 0,
-            padding = "";
-
-        if (node.match(/\{$/) || node.match(/\[$/)) {
-            indent = 1;
-        } else if (node.match(/\}/) || node.match(/\]/)) {
-            if (pad !== 0) {
-                pad -= 1;
-            }
-        } else {
-            indent = 0;
-        }
-
-        for (i = 0; i < pad; i++) {
-            padding += PADDING;
-        }
-
-        formatted += padding + node + "\r\n";
-        pad += indent;
-    });
-
-    return formatted;
-}
-
 var _url = "http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'"
 var parsed = '.$parsed.'
 
@@ -464,8 +383,8 @@ $("#callBtn").click(function(){
             try {
                 parsed = JSON.parse(data)
                 //console.log(parsed)
-                $("#response_content").html(formatJson(parsed.response))
-                $("#bin_content").html(formatJson(parsed.bin_data))
+                $("#response_content").html(JSON.stringify(parsed.response,null,6))
+                $("#bin_content").html(JSON.stringify(parsed.bin_data,null,6))
                 $("#status_content").html(JSON.stringify(status))
                 $("#raw_content").html(data)
                 $("#rpctab a[href=\\"#response\\"]").tab("show")
@@ -487,6 +406,13 @@ $("#saveCase").click(function(){
 
     saveCurrentCase()
 
+})
+
+$("#arguments").blur(function(){
+  var text = $("#arguments").val()
+  console.log(text)
+  var jsondata = JSON.parse(text)
+   $("#arguments").val(JSON.stringify(jsondata,null,6))
 })
 
 function hideNotice()
@@ -534,7 +460,7 @@ function quicklinkRender()
         item = items[i]
         html += "<li>"
         html += \'<div class="btn-group" role="group">\'
-        html += \'<a href="\'+item.link+\'" class="btn btn-default">\'+item.name+\'</a><a  href="javascript:;"  onclick="quicklinkRemove(\'+i+\')" class="btn btn-danger  btn-default">x</a></div></li>\'
+        html += \'<a href="\'+item.link+\'" class="btn btn-default btn-xs ">\'+item.name+\'</a><a  href="javascript:;"  onclick="quicklinkRemove(\'+i+\')" class="btn btn-danger btn-xs  btn-default">x</a></div></li>\'
     }
     $("#quicklinks").html(html)
 }
@@ -600,3 +526,4 @@ if($action == "") {
 }else{
     call();
 }
+
